d324329a7d905167352bf18ec7f1b18fe6de69c5
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2022-12-16 17:40:15 +0700

    add: top section (no responsive)

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,9 +1,22 @@
-export const TopSection = () => {
+export const TopSection = ({onClickNext}) => {
   return (
     <>
-      <div className="bg-bloodred-100">
-        This is the top section.
+      <div className="min-h-[100vh] object-contain text-white select-none bg-gradient-to-b from-[#991D1D] to-[#170606]">
+        <div className="flex justify-evenly place-content-evenly min-h-[100vh] pb-5 z-1 bg-[position:bottom] bg-no-repeat bg-[size:100vw_110%] bg-[url('/public/assets/combinedcurve.svg')]">
+          <img src="/public/assets/lgstar1.svg" className="" />
+          <div className="flex flex-col items-center">
+            <img src="/public/assets/roadtoengineer.png" className="max-h-[40vh] pt-[100px]" />
+            <img src="/public/assets/building.svg" className="max-h-[50vh]" />
+            <div onClick={onClickNext} className="flex flex-col font-bold text-center items-center cursor-pointer text-white ">
+              Read More
+              <img src="/public/assets/downarrow.svg" className="p-1"/>
+            </div>
+          </div>
+          <img src="/public/assets/lgstar2.svg" className="" />
+        </div>
       </div>
+      
+      <img src="/public/assets/bottomcurve.svg" className="w-[105vw] -translate-y-1" />
     </>
   )
 }
