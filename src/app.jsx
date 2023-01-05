f20f6b48284e1dad6aa4fd2939c7e19ddf855870
Qandle <unmistakework@hotmail.com>
2023-01-05 11:20:32 +0700

    feat: signup style + responsive

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { FrontPage } from './pages/FrontPage';
 import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './assets/css/app.css';
 
@@ -11,6 +12,7 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<FrontPage/>} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<Navigate to="/" />} />        
         </Routes>
       {/* </AuthProvider> */}
