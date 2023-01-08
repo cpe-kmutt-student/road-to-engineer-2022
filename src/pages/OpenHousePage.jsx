a8de56f0d703799a652d177f58443994f1936adc
Pannapat Sittiopasanun <nut.pannapat@gmail.com>
2023-01-08 23:45:36 +0700

    feat: add openhouse page route (#37)

---
+++ src/pages/OpenHousePage.jsx
@@ -0,0 +1,22 @@
+import { Hero } from '../components/Hero';
+import { Schedule } from '../components/Schedule';
+import { EventSection } from '../components/EventSection';
+import { Estamp } from '../components/Estamp';
+import { Faq } from '../components/Faq';
+import { Maps } from '../components/Maps';
+import { Another } from '../components/Another';
+import { Footer } from '../components/Footer';
+
+export const OpenHousePage = () => {
+  return (
+    <div>
+      <Hero />
+      <Schedule />
+      <EventSection />
+      <Estamp />
+      <Another />
+      <Faq />
+      <Maps />
+    </div>
+  );
+}
