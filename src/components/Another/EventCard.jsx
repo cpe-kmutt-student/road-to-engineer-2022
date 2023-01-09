fbb75c709781849624efdcbbf033c489c539957b
Chaiyapat Oam <83873103+ChaiyapatOam@users.noreply.github.com>
2023-01-09 16:13:29 +0700

    :sparkles:feat: Map Section (#49)

--- src/components/Another/EventCard.jsx
+++ src/components/Another/EventCard.jsx
@@ -1,7 +1,7 @@
 export const EventCard = ({ title, image, detail }) => {
   return (
     <div className="card-image bg-black">
-      <img src={image} alt="" className="opacity-30	" />
+      <img src={image} alt="" className="opacity-30	aspect-square" />
       <div className="card-image-text">
         <h2 className=" text-white text-3xl">{title}</h2>
         <p className=" text-white">{detail}</p>
